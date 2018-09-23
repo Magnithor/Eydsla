09552857cdc0b72c845571c87347893642f69cb6
Magni Þór Birgisson <magni.thor@live.com>
2018-09-23 18:11:43 +0000

    localStorage

---
+++ src/app/service/local-storage.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  public getValue(key:string):string {    
+    return localStorage.getItem(key);
+  }
+
+  public getValueObject(key:string):object {
+    return JSON.parse(this.getValue(key));
+  }
+
+  public setValue(key:string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  public setValueObject(key:string, value:object){
+    this.setValue(key, JSON.stringify(value));
+  }
+}
